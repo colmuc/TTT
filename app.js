83e50b415b5bb725232e4369d6a1a187a8f272fd
Collins Mucyo <collinsmucyo@gmail.com>
2019-04-16 13:20:33 -0500

    Improved Logic

--- app.js
+++ app.js
@@ -46,33 +46,36 @@ showBoard = () => {
 startGame = () => {
   readline.question("What is your move", input => {
     for (i = 0; i < 5; i++) {
-      if (
-        input == "0" ||
-        input == "1" ||
-        input == "2" ||
-        input == "3" ||
-        input == "4" ||
-        input == "5" ||
-        input == "6" ||
-        input == "7" ||
-        input == "8"
-      ) {
-        console.log("Good Move, move again");
-        move(input);
-      } else if (input == "-1") {
-        gameOver();
-        break;
-      } else {
-        console.log("Wrong move, please enter a number between 0 and 8");
-        startGame();
-        break;
-      }
+      move();
     }
   });
 };
 
 move = input => {
+  if (
+    input == "0" ||
+    input == "1" ||
+    input == "2" ||
+    input == "3" ||
+    input == "4" ||
+    input == "5" ||
+    input == "6" ||
+    input == "7" ||
+    input == "8"
+  ) {
+    console.log("Good Move, move again");
+    move(input);
+  } else if (input == "-1") {
+    gameOver();
+  } else {
+    console.log("Wrong move, please enter a number between 0 and 8");
+    move();
+  }
+};
+
+displayMove = input => {
   if (input == "0") {
+    board[0] = "X";
   } else if (input == "1") {
   } else if (input == "2") {
   } else if (input == "3") {
