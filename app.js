43015ec2281218d9c80115dfa5778d37ab350f24
Collins Mucyo <collinsmucyo@gmail.com>
2019-04-16 12:07:54 -0500

    Created Board

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 play = () => {
-  getNumberofPlayer();
+  //getNumberofPlayer();
   showBoard();
   console.log("Enter 0-8");
 };
+const player1 = "X";
+const player2 = "O";
+const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+showBoard = () => {
+  console.log(
+    " " +
+      board[0] +
+      " |" +
+      " " +
+      board[1] +
+      " |" +
+      " " +
+      board[2] +
+      "\n---+---+---\n" +
+      " " +
+      board[3] +
+      " |" +
+      " " +
+      board[4] +
+      " |" +
+      " " +
+      board[5] +
+      "\n---+---+---\n" +
+      " " +
+      board[6] +
+      " |" +
+      " " +
+      board[7] +
+      " |" +
+      " " +
+      board[8]
+  );
+};
+
+play();
